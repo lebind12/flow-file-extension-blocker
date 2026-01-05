0f8bf177582afe07b76cae18e1a8f51a7c8df351
이우진 <52599002+lebind12@users.noreply.github.com>
2026-01-05 19:17:51 +0900

    [FE] BE 배포 URL 적용

--- frontend/src/api/extensionApi.js
+++ frontend/src/api/extensionApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://inner-vida-lebind12-1746d7bc.koyeb.app/api',
   headers: {
     'Content-Type': 'application/json'
   }
